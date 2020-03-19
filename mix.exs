14ba7e562ce7ac9d475c1b376e608ff569a92afc
Paul Engel <pm_engel@icloud.com>
2020-03-19 22:14:18 +0100

    Support Elixir >= 1.5.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExUnited.MixProject do
     [
       app: :ex_united,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: ">= 1.5.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
