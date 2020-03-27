39bf31ed0db6cb2e31ba82957606e625d2f2535f
Paul Engel <pm_engel@icloud.com>
2020-03-27 01:22:09 +0100

    Set warnings as errors

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule ExUnited.MixProject do
       app: :ex_united,
       version: "0.1.0",
       elixir: ">= 1.5.0",
+      elixirc_options: [warnings_as_errors: true],
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: dialyzer(),
